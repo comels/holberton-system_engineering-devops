dacdcbce549e675e1bc25ee1c1dec811fff4e246
Come Le Sauter <come.l.s@hotmail.fr>
2022-09-28 13:39:44 +0200

    strace

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Using strace, find out why Apache is returning a 500 error.
+
+exec { 'strace':
+  command => 'sed -i s/phpp/php/ /var/www/html/wp-settings.php',
+  path    =>  [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ]
+}
